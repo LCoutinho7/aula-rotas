92b8fe3fab385d0ed5d83d6302d00c926b041ad7
LCoutinho7 <rm551617@fiap.com.br>
2023-09-05 21:18:31 -0300

    Finalizando páginas jsx e css

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Home from './routes/Home/index.jsx'
+import Produtos from './routes/Produtos/index.jsx'
+import EditarProduto from './routes/EditarProduto/index.jsx'
+import Error from './routes/Error/index.jsx'
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/produtos",
+        element: <Produtos />,
+      },
+      {
+        path: "/produto/editar/:id", 
+        element: <EditarProduto/>,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
